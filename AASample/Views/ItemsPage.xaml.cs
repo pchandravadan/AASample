a879c248c8504d2701570dec0b2a75d56906fba7
Alexey Strakh <alexey.strakh@microsoft.com>
2020-06-05 17:58:49 -0700

    fix hotreload with on orientation change event

--- AASample/Views/ItemsPage.xaml.cs
+++ AASample/Views/ItemsPage.xaml.cs
@@ -19,6 +19,12 @@ namespace AASample.Views
         private double width;
         private double height;
 
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            ApplyStyleOnOrientationChanged();
+        }
+
         protected override void OnSizeAllocated(double width, double height)
         {
             base.OnSizeAllocated(width, height);
@@ -26,31 +32,37 @@ namespace AASample.Views
             {
                 this.width = width;
                 this.height = height;
-                if (width > height)
-                {
-                    Resources["gridStyle"] = Resources["gridLandscape"];
-
-                    innerGrid.RowDefinitions.Clear();
-                    innerGrid.ColumnDefinitions.Clear();
-                    innerGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
-                    innerGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
-                    innerGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
-                    innerGrid.Children.Remove(controlsGrid);
-                    innerGrid.Children.Add(controlsGrid, 1, 0);
-                }
-                else
-                {
-                    Resources["gridStyle"] = Resources["gridPotrait"];
-
-                    innerGrid.RowDefinitions.Clear();
-                    innerGrid.ColumnDefinitions.Clear();
-                    innerGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
-                    innerGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Auto) });
-                    innerGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
-                    innerGrid.Children.Remove(controlsGrid);
-                    innerGrid.Children.Add(controlsGrid, 0, 1);
-                }
+                ApplyStyleOnOrientationChanged();
             }
         }
+
+        private void ApplyStyleOnOrientationChanged()
+        {
+            if (width > height)
+            {
+                Resources["gridStyle"] = Resources["gridLandscape"];
+
+                innerGrid.RowDefinitions.Clear();
+                innerGrid.ColumnDefinitions.Clear();
+                innerGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+                innerGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+                innerGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+                innerGrid.Children.Remove(controlsGrid);
+                innerGrid.Children.Add(controlsGrid, 1, 0);
+            }
+            else
+            {
+                Resources["gridStyle"] = Resources["gridPotrait"];
+
+                innerGrid.RowDefinitions.Clear();
+                innerGrid.ColumnDefinitions.Clear();
+                innerGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+                innerGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Auto) });
+                innerGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+                innerGrid.Children.Remove(controlsGrid);
+                innerGrid.Children.Add(controlsGrid, 0, 1);
+            }
+        }
+    }
     }
 }
